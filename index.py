3dde701431aeaeb5fda9143ffd842e486729e2ba
Oleksandr Shkovyra <oleksandr.shkovyra@gmail.com>
2020-02-05 11:30:32 +0200

    final implementation

--- index.py
+++ index.py
@@ -25,8 +25,7 @@ def check_word_in_state(word, list_name):
     return(word in list_name)
 
 def update_state(word, list_name):
-    if word not in list_name and check_word_in_dict(word) is True:
-        return(list_name.append(word))
+    return(list_name.append(word))
 
 def check_list_not_empty(user_input, list_name):
     if len(list_name) >= 1:
@@ -37,10 +36,7 @@ def check_that_list_empty(user_input, list_name):
         return(True)
 
 def check_word_to_rules(word, list_name):
-    if get_last_letter(list_name[-1]) == get_first_letter(word):
-        update_state(word, state)
-    else:
-        print('Please, choose another word!')
+    return(get_last_letter(list_name[-1]) == get_first_letter(word))
 
 def words_list():
     return(words.raw().strip())
@@ -56,25 +52,18 @@ def check_list_state(list_name):
 
 def game(user_input, state):
     print('Your word is "' + user_input + '".' )
-    if check_word_in_state(user_input, state) is True:
-        print('This word in the state!')
 
-    if check_word_in_dict(user_input) is False:
-        print("This word not in the dictionary!")
-
-    if check_that_list_empty(user_input, state) is True:
-        update_state(user_input, state)
-
-    if check_list_not_empty(user_input, state) is True:
-        check_word_to_rules(user_input, state)
-
-#get_update_dictionary()
-
-#for i in list(range(10)):
-#    game(get_user_input(),state)
-#check_list_state(state)
+    if check_that_list_empty(user_input, state) and check_word_in_dict(user_input):
+        return(update_state(user_input, state))
+    elif check_list_not_empty(user_input, state) and check_word_in_dict(user_input) and not check_word_in_state(user_input, state) and check_word_to_rules(user_input, state):
+        return(update_state(user_input, state))
+    else:
+        return('else')
 
 for i in list(range(10)):
-    game(get_user_input(),state)
+    while game(get_user_input(),state) == 'else':
+        print('This word incorrect. Please choose another one.')
+
     game(bot_word(get_last_letter(state[-1])),state)
+
 check_list_state(state)
